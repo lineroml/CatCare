d4fbe7d54a4d441f7f1167d4e058260bb0dfe8d2
Luisa Escobar <lineroml@uninorte.edu.co>
2020-11-27 07:29:23 -0500

    idk

--- src/game.js
+++ src/game.js
@@ -4,11 +4,7 @@ import { ServerCreator } from '/ServerCreator.js';
 import { MPtest } from '/MPtest.js'
 import { PauseMenu } from '/PauseMenu.js';
 import { PlayersInfo } from '/PlayersInfo.js';
-<<<<<<< HEAD
-import { winner } from '/winner.js';
-=======
 import { Winner } from '/Winner.js';
->>>>>>> b069cd67814614816731b440f9c1d209b2d4afad
 const config = {
     type: Phaser.AUTO,
     width: 800,
